ddfee40bbb8df2f6108d86c759554d3aef8f87ab
user <user@10.0.0.4>
2017-12-09 11:18:01 +0200

    First Commit Test

---
+++ src/test/java/demo/TestNG.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class TestNG {
+	
+	@Test
+	public void launch()
+
+	{
+		
+		System.out.println("Hello World");
+		
+		
+		
+		
+	}
+}
