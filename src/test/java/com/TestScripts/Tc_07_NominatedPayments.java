e9201008b25007d08214cc2d70de99557fe9c807
SriniR <SriniR@SriniR_Dell.solarsystem.ioh.co.za>
2018-01-10 10:22:15 +0200

    Tc_07

---
+++ src/test/java/com/TestScripts/Tc_07_NominatedPayments.java
@@ -0,0 +1,29 @@
+package com.TestScripts;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.GenericFunctions.GenericMethods;
+import com.ScreenFunctions.Dashboard;
+import com.ScreenFunctions.Login;
+import com.ScreenFunctions.NominatedPayments;
+
+public class Tc_07_NominatedPayments extends GenericMethods{
+
+	@Test
+	public static void evaluateTestcase() throws Exception {
+		GenericMethods.LaunchBrowser("chrome");
+         Login lgn=PageFactory.initElements(driver, Login.class);
+         
+         Dashboard db=PageFactory.initElements(driver, Dashboard.class);
+
+         NominatedPayments np=PageFactory.initElements(driver, NominatedPayments.class);
+
+		lgn.launch_Application();
+		db.EntitySearch(); 
+		np.NominatedPayments();
+
+		
+
+	}
+
+}
